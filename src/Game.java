c0615ff802b08d7ed9a9e93b5ced3be2284af66b
pbm222 <innapbm@gmail.com>
2019-11-30 13:24:36 +0300

    test message

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,88 @@
+import java.util.ArrayList;
 
 public class Game {
 
+	private String userWord;
+	private String guessedWord;
+	//private int wordLength;
+	
+	public Game(String userWord, String guessedWord) {
+		this.setUserWord(userWord);
+		this.guessedWord = guessedWord;
+	}
+
+	
+	public String getUserWord() {
+		return userWord;
+	}
+
+	public void setUserWord(String userWord) {
+		this.userWord = userWord;
+	}
+
+
+	public String getGuessedWord() {
+		return guessedWord;
+	}
+
+	public void setGuessedWord(String guessedWord) {
+		this.guessedWord = guessedWord;
+	}
+	
+	public int getBulls() {
+		int bulls = 0;
+		for(int i=0; i<userWord.length(); i++) {
+			char letterGuessed = guessedWord.charAt(i);
+			char letterUser = userWord.charAt(i);
+			if(letterGuessed == letterUser) {
+				bulls++;
+			}
+		}
+		return bulls;
+	}
+	
+	public ArrayList<Integer> bullPosition() {
+		ArrayList<Integer> bullPosition= new ArrayList<>();
+		for(int i=0; i<userWord.length(); i++) {
+			char letterGuessed = guessedWord.charAt(i);
+			char letterUser = userWord.charAt(i);
+			if(letterGuessed == letterUser) {
+				bullPosition.add(i+1);
+			}
+		}
+		return bullPosition;
+	}
+	
+	public int getCows() {
+		Game game = new Game(userWord, guessedWord);
+		int cows = 0;
+		
+		
+		return cows;
+	}
+	
+	public boolean win() {
+		Game game = new Game(userWord, guessedWord);
+		boolean winOrnot;
+		int bulls = game.getBulls();
+		if(bulls == 3) {
+			winOrnot = true;
+		}else {
+			winOrnot = false;
+		}
+		return winOrnot;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
