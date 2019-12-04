d20ff107c8df3e3dc527833e3dc58e77e5b5c7f0
pbm222 <innapbm@gmail.com>
2019-12-04 22:09:36 +0300

    Final project

--- src/Reader.java
+++ src/Reader.java
@@ -1,6 +1,7 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.List;
 import java.util.Random;
 
--- src/testMain.java
+++ src/testMain.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 
+import static com.sun.org.apache.xerces.internal.impl.dv.util.Base64.encode;
+
 public class testMain {
 
     public static void main(String[] args) {
+
+
         System.out.println("Hello, my dear friend! Let's play a game: Cows and Bulls! ");
         System.out.println("\n");
         System.out.println("Please enter the number of letters in a word you want to guess:");
@@ -18,13 +22,27 @@ public class testMain {
         Game game = new Game(userWord, guessedWord, wordLength);
 
         do {
-            System.out.println("Bulls: " + game.getBulls().size());
-            System.out.println("Cows: " + game.getCows());
 
-            System.out.println("Try harder! New try : ");
-            ab = new Scanner(System.in);
-            userWord = ab.nextLine();
-            game.setUserWord(userWord);
+            if (game.getBulls().size() >= 1 && game.getBulls().size() < wordLength){
+                for(int i=0; i<wordLength; i++){
+                    if (game.getBulls().contains(i)){
+                        System.out.print(userWord.charAt(i));
+                    }else{
+                        System.out.print("*");
+                    }
+                }
+                System.out.println("");
+                System.out.println("Cows: " + game.getCows());
+            }else {
+                System.out.println("Bulls: " + game.getBulls().size());
+                System.out.println("Cows: " + game.getCows());
+            }
+            System.out.println("");
+           System.out.println("Try harder! New try : ");
+           ab = new Scanner(System.in);
+           userWord = ab.nextLine();
+           game.setUserWord(userWord);
+
 
         } while (!game.win());
 
